d60faea6011bbdadd732ab1023bcd961ea7f8aab
sz-p <sz_p@outlook.com>
2022-12-30 17:51:10 +0800

    feat: add visitor api to visit all tag under selected svgdom

--- src/core.ts
+++ src/core.ts
@@ -1,4 +1,5 @@
 export const svgVisitor = function (svgDom: SVGElement, options: {
+  visitor?: (dom: Element) => void,
   svgVisitor?: (dom: SVGElement) => void,
   pathVisitor?: (dom: SVGPathElement) => void,
   circleVisitor?: (dom: SVGCircleElement) => void,
@@ -14,8 +15,12 @@ export const svgVisitor = function (svgDom: SVGElement, options: {
     if (!dom) continue;
     const functionKey = `${dom.tagName}Visitor`
     const fc = options[functionKey]
+    const visitor = options.visitor
     if (fc && typeof fc === 'function') {
       fc(dom)
+    }
+    if (typeof visitor === 'function') {
+      visitor(dom)
     }
     if (dom.children) {
       domStack = domStack.concat(...dom.children as unknown as Array<SVGElement>)
